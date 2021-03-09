a39e36f9087f2bfe6e3bdddf94cb0be57da42923
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2021-03-09 16:26:52 +0100

    exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,14 @@ import * as Structures from './structures'
 
 
 export const User = Structures.User;
+export const Manager = Structures.Manager;
+export const Base = Structures.Base;
+export const BaseStructure = Structures.BaseStructure;
+export const Collection = Structures.Collection;
+export const CollectionElement = Structures.CollectionElement;
+export const Color = Structures.Color;
+export const ColorTheme = Structures.ColorTheme;
+export const Language = Structures.Language;
+export const School = Structures.School;
+export const SchoolManager = Structures.SchoolManager;
+export const SchoolUserPermissions = Structures.SchoolUserPermissions;
